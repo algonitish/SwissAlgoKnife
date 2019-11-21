3fa34f5a033aec71840c58e7e5438efc545ac1f7
algonitish <45369850+algonitish@users.noreply.github.com>
2019-11-21 16:35:00 +0530

    Create

---
+++ secret/cred.py
@@ -0,0 +1,18 @@
+upstox_api_token        =''
+upstox_api_redirect_uri =''
+upstox_api_secret       =''
+upstox_api_key          =''
+
+APCA_API_KEY_ID = ''
+APCA_API_SECRET_KEY = ''
+APCA_API_BASE_URL = ''
+APCA_API_DATA_URL = ''
+
+str_ib_ip = '127.0.0.1'
+int_ib_port = 7497
+int_ib_uid = 101
+
+str_mysql_host = 'localhost'
+str_mysql_database = ''
+str_mysql_user = ''
+str_mysql_password = ''
